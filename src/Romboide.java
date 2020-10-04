23dfa9839806ae7af869a087ebc989748031a1f5
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-10-04 16:15:50 -0500

    Creación clase Romboide *Creación clase Romboide *Implementación de Romboide en Main

---
+++ src/Romboide.java
@@ -0,0 +1,13 @@
+public class Romboide extends Figura {
+    public Romboide(){
+    }
+
+    public Romboide(int base, int altura){
+        super(base,altura);
+    }
+
+    @Override
+    public float calcularArea(int base, int altura) {
+        return area=base*altura;
+    }
+}
