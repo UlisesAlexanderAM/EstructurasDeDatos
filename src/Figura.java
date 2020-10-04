f9b0b32aa31e0bdc749a37ce3e8f8142214c168f
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-10-04 12:10:27 -0500

    Creación de la clase padre "Figura".

---
+++ src/Figura.java
@@ -0,0 +1,33 @@
+public class Figura {
+    protected int base;
+    protected int altura;
+
+    public Figura(){
+    }
+
+    public Figura(int base, int altura){
+        this.base=base;
+        this.altura=altura;
+    }
+
+    public float calcularArea() {
+
+        return 0;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public int getBase() {
+        return base;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public void setBase(int base) {
+        this.base = base;
+    }
+}
