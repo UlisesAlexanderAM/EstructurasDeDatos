a75ca9b945f003f3b9198139e197541a5e7f2ce7
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-10-06 18:24:58 -0500

    Creación Main *Creación de la clase Main

---
+++ src/HerenciaElectrodomesticos/Main.java
@@ -0,0 +1,11 @@
+package HerenciaElectrodomesticos;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static ArrayList<Electrodomestico> electrodomesticos = new ArrayList<>(10);
+    public static void main(String[] args){
+
+    }
+}
