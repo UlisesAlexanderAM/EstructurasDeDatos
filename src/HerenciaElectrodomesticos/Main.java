b41ff014a0dfea933422b92baf61d0663c686679
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-10-06 20:59:20 -0500

    Precios totales *Obtención e impresión de los precios totales de los electrodomésticos, lavadoras y televisiones.

--- src/HerenciaElectrodomesticos/Main.java
+++ src/HerenciaElectrodomesticos/Main.java
@@ -1,7 +1,6 @@
 package HerenciaElectrodomesticos;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 import static LecturaEscritura.Imprimir.imprimir;
 
@@ -9,25 +8,29 @@ public class Main {
 
     public static ArrayList<Electrodomestico> electrodomesticos = new ArrayList<>(10);
     public static void main(String[] args){
-        int indiceLavadora=0, indiceTelevision=0;
+        int indiceLavadora=0, indiceTelevision=0, precioTotal=0, precioTotalTelevision=0,precioTotalLavadora=0;
         Lavadora lavadora1 = new Lavadora();
         Television television1 = new Television();
 
         electrodomesticos.add(lavadora1);
         electrodomesticos.add(television1);
 
-        Iterator<Electrodomestico> iteradorElectrodomesticos = electrodomesticos.iterator();
-
         for (Electrodomestico electrodomestico : electrodomesticos){
             int indice;
             if (electrodomestico.getClass().getSimpleName().equals("Lavadora")){
                 indiceLavadora+=1;
                 indice=indiceLavadora;
+                precioTotalLavadora+=electrodomestico.precioFinal();
             }else {
                 indiceTelevision+=1;
                 indice=indiceTelevision;
+                precioTotalTelevision+=electrodomestico.precioFinal();
             }
             imprimir(String.format("El precio de %s es de: %s \n", electrodomestico.toString(indice), electrodomestico.precioFinal()));
+            precioTotal+=electrodomestico.precioFinal();
         }
+        imprimir("\nEl precio total de los electrodomesticos es: "+precioTotal+
+                "\nEl precio total de las lavadoras es: "+precioTotalLavadora+
+                "\nEl precio total de las televisiones es: "+precioTotalTelevision);
     }
 }
