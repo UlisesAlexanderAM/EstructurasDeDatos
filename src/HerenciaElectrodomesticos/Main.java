c197d8ecb766d70a788ca7a530a9512e105490e3
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-10-06 20:20:40 -0500

    Creación Television *Creación de la clase hija Television *Implementación de Television en Main

--- src/HerenciaElectrodomesticos/Main.java
+++ src/HerenciaElectrodomesticos/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static ArrayList<Electrodomestico> electrodomesticos = new ArrayList<>(10);
     public static void main(String[] args){
+        Television television1 = new Television();
 
+        electrodomesticos.add(television1);
     }
 }
---
+++ src/HerenciaElectrodomesticos/Television.java
@@ -0,0 +1,64 @@
+package HerenciaElectrodomesticos;
+
+public class Television extends Electrodomestico{
+    private int resolucion;
+    private boolean sintonizadorTDT;
+    private final int resolucionPorDefecto = 20;
+    private final boolean sintonizadorTDTPorDefecto = false;
+
+    public Television() {
+        resolucion = resolucionPorDefecto;
+        sintonizadorTDT = sintonizadorTDTPorDefecto;
+    }
+
+    public Television(int resolucion, boolean sintonizadorTDT) {
+        this.resolucion = resolucion;
+        this.sintonizadorTDT = sintonizadorTDT;
+    }
+
+    public Television(float precioBase, float peso) {
+        super(precioBase, peso);
+        resolucion = resolucionPorDefecto;
+        sintonizadorTDT = sintonizadorTDTPorDefecto;
+    }
+
+    public Television(float precioBase, float peso, int resolucion, boolean sintonizadorTDT) {
+        super(precioBase, peso);
+        this.resolucion = resolucion;
+        this.sintonizadorTDT = sintonizadorTDT;
+    }
+
+    public Television(float precioBase, String color, char consumoEnergetico, float peso, int resolucion, boolean sintonizadorTDT) {
+        super(precioBase, color, consumoEnergetico, peso);
+        this.resolucion = resolucion;
+        this.sintonizadorTDT = sintonizadorTDT;
+    }
+
+    public Television(float precioBase, String color, char consumoEnergetico, float peso) {
+        super(precioBase, color, consumoEnergetico, peso);
+        resolucion = resolucionPorDefecto;
+        sintonizadorTDT = sintonizadorTDTPorDefecto;
+    }
+
+    @Override
+    public float precioFinal() {
+        float precio = 0;
+
+        if (resolucion>=40){
+            precio +=precioBase*0.3;
+        }
+        if (sintonizadorTDT=true){
+            precio+=50;
+        }
+
+        return super.precioFinal()+precio;
+    }
+
+    public int getResolucion() {
+        return resolucion;
+    }
+
+    public boolean isSintonizadorTDT() {
+        return sintonizadorTDT;
+    }
+}
