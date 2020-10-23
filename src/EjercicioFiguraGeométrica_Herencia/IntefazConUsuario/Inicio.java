b8b1ffb040c814955633077b7aa59ccfd5ed61ad
Ulises Alexander Arguelles Monjaraz <ulises_alexander_am@protonmail.com>
2020-10-23 00:19:12 -0500

    Modificaciones provisionales

--- src/EjercicioFiguraGeométrica_Herencia/IntefazConUsuario/Inicio.java
+++ src/EjercicioFiguraGeométrica_Herencia/IntefazConUsuario/Inicio.java
@@ -10,6 +10,7 @@ public class Inicio {
 		ventana.setTitle("Figuras Geometricas");
 		JPanel jPanel = new JPanel();
 		JComponent pestañaRectangulo = new PestañaRectangulo().$$$getRootComponent$$$();
+		JComponent pestañaTriangulo = new PestañaTriangulo().$$$getRootComponent$$$();
 		ventana.setContentPane(jPanel);
 		ventana.setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
@@ -17,9 +18,15 @@ public class Inicio {
 		gbc.gridy=0;
 		gbc.fill = GridBagConstraints.BOTH;
 		jPanel.add(pestañaRectangulo,gbc);
+		gbc.gridx=1;
+		gbc.gridy=0;
+		gbc.fill = GridBagConstraints.BOTH;
+		Dimension dimension = new Dimension(250,500);
+		jPanel.add(pestañaTriangulo,gbc);
+		ventana.setPreferredSize(dimension);
+		ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		ventana.setVisible(true);
-//		ventana.pack();
-
+		ventana.pack();
 	}
 
 }
